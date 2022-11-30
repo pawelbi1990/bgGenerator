66f7df24730c0a382f79e5e20e0cca1119ff28cf
pawelbi1990 <114158452+pawelbi1990@users.noreply.github.com>
2022-11-30 12:15:17 +0100

    Add files via upload
    implemented logic and basic layout

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+function changeBg() {
+    body.style.background = 
+    "linear-gradient(to right, "
+    +color1.value
+    +", "
+    +color2.value
+    +")";
+}
+
+color1.addEventListener("input", changeBg);
+
+color2.addEventListener("input", changeBg);
